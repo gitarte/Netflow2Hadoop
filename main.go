56aec89ffca32c2e823fd0159617494bcf906e1d
gitarte <art-e@o2.pl>
2018-03-28 16:46:54 +0200

    add saveing into file

--- main.go
+++ main.go
@@ -1,35 +1,81 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
+	"os"
+	"strconv"
 )
 
 func main() {
-	ServerAddr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:9995")
+	//	reading commandline arguments
+	host := os.Args[1] //	listen address
+	port := os.Args[2] //	listen port
+	//dstf := os.Args[3] //	directory for result files
+
+	chanOfFlows := make(chan NetFlowV5)
+	chanOfSlices := make(chan []NetFlowV5)
+
+	//	creating UDP socket that will accept netlow traffic
+	ServerAddr, _ := net.ResolveUDPAddr("udp", host+":"+port)
 	ServerConn, _ := net.ListenUDP("udp", ServerAddr)
 	defer ServerConn.Close()
+	fmt.Printf("Socket listening on %s:%s\n", host, port)
+
+	//	This goroutine accumulates netlows till it collects 500000 of them.
+	//	Afterwards it sends accumulated data into another goroutine that saves it into file
+	go func(channel chan NetFlowV5) {
+		for {
+			var flowsArray [500000]NetFlowV5
+
+			i := 0
+			for i < 500000 {
+				flowsArray[i] = <-channel
+				i++
+			}
+
+			chanOfSlices <- flowsArray[:]
+		}
+	}(chanOfFlows)
+
+	//	This goroutine stores given data into file
+	go func(channel chan []NetFlowV5) {
+		fileCount := 0
+		for {
+			//	create new file
+			fileCount++
+			flowsArray := <-channel
+			f, _ := os.Create("./" + strconv.Itoa(fileCount) + "flow")
+			defer f.Close()
+
+			//	feed file with JSON's
+			for _, flow := range flowsArray {
+				//	struct to json
+				b, _ := json.Marshal(&flow)
+				f.WriteString(string(b) + "\n")
+			}
+		}
+	}(chanOfSlices)
 
 	for {
+		//	waiting for single transmission from netflow collector
 		buf := make([]byte, 4096)
 		n, _, _ := ServerConn.ReadFromUDP(buf)
-		if n < 24 {
-			//	panic("malformed record")
-			continue
-		}
-		go func(buf []byte, n int) {
-			var flow NetFlowV5
 
+		//	handling recaived data in separate goroutine
+		go func(buf []byte, n int, channel chan NetFlowV5) {
+			//	create new container for data
+			var flow NetFlowV5
 			//	Decode NetFlow v5 header
 			flow.Header = DecodeHeader(buf)
-
 			//	Decode NetFlow v5 records
 			for c := uint16(0); c < flow.Header.Count; c++ {
-				// store decoded reckord
 				flow.Records = append(flow.Records, DecodeRecord(buf, c))
 			}
 
-			fmt.Printf("%+v", flow)
-		}(buf, n)
+			//	sending decoded netflow transmission into accumulating goroutine
+			channel <- flow
+		}(buf, n, chanOfFlows)
 	}
 }
