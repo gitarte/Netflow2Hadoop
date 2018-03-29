b7efc3651c48c6c3b90797a67f66c92646df03f9
gitarte <art-e@o2.pl>
2018-03-29 09:37:01 +0200

    make output JSON always valid

--- main.go
+++ main.go
@@ -8,12 +8,22 @@ import (
 	"strconv"
 )
 
+type config struct {
+	host  string //	listen address
+	port  string //	listen port
+	chunk int    //	the size of accumulator
+	dest  string //	destination directory
+
+}
+
+var c config
+
 func main() {
 	//	Reading commandline arguments
-	host := os.Args[1]                   //	listen address
-	port := os.Args[2]                   //	listen port
-	chunk, _ := strconv.Atoi(os.Args[3]) //	the size of accumulator
-	dest := os.Args[4]                   //	destination directory
+	c.host = os.Args[1]                   //	listen address
+	c.port = os.Args[2]                   //	listen port
+	c.chunk, _ = strconv.Atoi(os.Args[3]) //	the size of accumulator
+	c.dest = os.Args[4]                   //	destination directory
 
 	//	Creating channel to pass netflow data betwean goroutines
 	chanOfFlows := make(chan NetFlowV5)
@@ -21,14 +31,14 @@ func main() {
 	//	Bringing to life infinite goroutine that accumulates netlows.
 	//	It collects about 128MB of data and afterwards it sends this chunk
 	//	into another goroutine that saves it into file
-	go accumulate(chanOfFlows, dest, chunk)
+	go accumulate(chanOfFlows)
 
 	//	Creating UDP socket that will infinitely accept netlow traffic
 	//	Each incomming message is handled in separate goroutine
-	ServerAddr, _ := net.ResolveUDPAddr("udp", host+":"+port)
+	ServerAddr, _ := net.ResolveUDPAddr("udp", c.host+":"+c.port)
 	ServerConn, _ := net.ListenUDP("udp", ServerAddr)
 	defer ServerConn.Close()
-	fmt.Printf("Socket is listening on %s:%s\n", host, port)
+	fmt.Printf("Socket is listening on %s:%s\n", c.host, c.port)
 	for {
 		//	waiting for single transmission from netflow collector
 		buf := make([]byte, 4096)
@@ -53,33 +63,35 @@ func decodeDatagrams(buf []byte, n int, channel chan NetFlowV5) {
 	channel <- flow
 }
 
-func accumulate(channel chan NetFlowV5, dest string, chunk int) {
+func accumulate(channel chan NetFlowV5) {
 	fileCount := 0
 	for {
 		//	prepare container for current data chunk
-		var flowsArray []NetFlowV5
+		var sliceOfFlows []NetFlowV5
 		fileCount++
 
 		//	accumulate data
 		i := 0
-		for i < chunk {
-			flowsArray = append(flowsArray, <-channel)
+		for i < c.chunk {
+			sliceOfFlows = append(sliceOfFlows, <-channel)
 			i++
 		}
 
 		//	This goroutine stores given data into file
-		go save(flowsArray, dest, fileCount)
+		go save(sliceOfFlows, fileCount)
 	}
 }
 
-func save(flowsArray []NetFlowV5, dest string, fileCount int) {
+func save(sliceOfFlows []NetFlowV5, fileCount int) {
 	//	create new file
-	f, _ := os.Create(dest + "/" + strconv.Itoa(fileCount) + ".flow")
+	f, _ := os.Create(c.dest + "/" + strconv.Itoa(fileCount) + ".flow")
 	defer f.Close()
 
-	//	feed file with JSON's
-	for _, flow := range flowsArray {
+	//	feed file with JSON array of decoded flows
+	f.WriteString("[")
+	for _, flow := range sliceOfFlows {
 		b, _ := json.Marshal(&flow)
-		f.WriteString(string(b) + "\n")
+		f.WriteString(string(b) + ",")
 	}
+	f.WriteString("{}]") //	dirty trick that makes JSON always valid
 }
