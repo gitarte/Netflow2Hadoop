af7cd2b5df1317eb1c4dcbd9eaf6f12bb60a8dba
gitarte <art-e@o2.pl>
2018-04-04 16:54:57 +0200

    major refactoring

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 )
 
 type config struct {
-	host  string //	listen address
-	port  string //	listen port
-	chunk int    //	the size of accumulator
-	dest  string //	destination directory
+	host      string //	listen address
+	port      string //	listen port
+	chunkSize int    //	the size of accumulator
+	dest      string //	destination directory
 }
 
 var c config
@@ -27,16 +27,16 @@ func main() {
 	if err != nil {
 		ExitOnError("main", err)
 	}
-	c.chunk = chunk
+	c.chunkSize = chunk
 	c.dest = os.Args[4] //	destination directory
 
 	//	Creating channel to pass netflow data betwean goroutines
-	chanOfFlows := make(chan NetFlowV5)
+	channel := make(chan []byte)
 
 	//	Bringing to life infinite goroutine that accumulates netlows.
 	//	It collects about 128MB of data and afterwards it sends this chunk
 	//	into another goroutine that saves it into file
-	go accumulate(chanOfFlows)
+	go accumulate(channel)
 
 	//	Creating UDP socket that will infinitely accept netlow traffic
 	//	Each incomming message is handled in separate goroutine
@@ -44,68 +44,57 @@ func main() {
 	if err != nil {
 		ExitOnError("ResolveUDPAddr", err)
 	}
-
 	ServerConn, err := net.ListenUDP("udp", ServerAddr)
 	if err != nil {
 		ExitOnError("ServerConn", err)
 	}
-
 	defer ServerConn.Close()
 	fmt.Printf("Socket is listening on %s:%s\n", c.host, c.port)
 	for {
 		//	waiting for single transmission from netflow collector
-		buf := make([]byte, 4096)
-		n, _, err := ServerConn.ReadFromUDP(buf)
+		datagram := make([]byte, 4096)
+		_, _, err := ServerConn.ReadFromUDP(datagram)
 		if err != nil {
 			LogOnError("ServerConn", err)
 			continue
 		}
-		//	decoding recaived data in separate goroutine
-		go decodeDatagrams(buf, n, chanOfFlows)
+		//	Sending data for for further processing
+		//	which takes place in "accumulate" goroutine
+		channel <- datagram
 	}
 }
 
-func decodeDatagrams(buf []byte, n int, channel chan NetFlowV5) {
-	defer RecoverAnyPanic("decodeDatagrams")
-
-	//	create new container for data
-	var flow NetFlowV5
-	//	Decode NetFlow v5 header
-	flow.Header = DecodeHeader(buf)
-	//	Decode NetFlow v5 records
-	for c := uint16(0); c < flow.Header.Count; c++ {
-		flow.Records = append(flow.Records, DecodeRecord(buf, c))
-	}
-
-	//	sending decoded netflow into accumulating goroutine
-	channel <- flow
-}
-
-func accumulate(channel chan NetFlowV5) {
+func accumulate(channel chan []byte) {
 	defer RecoverAnyPanic("accumulate")
 
-	fileCount := 0
+	fileCount := 0               // integer counter of output files
+	maxInt := int(^uint(0) >> 1) //	compute maximum value of type int
+
 	for {
 		//	prepare container for current data chunk
-		sliceOfFlows := make([]NetFlowV5, c.chunk)
-		fileCount++
+		chunk := make([][]byte, c.chunkSize)
 
 		//	accumulate data
 		i := 0
-		for i < c.chunk {
+		for i < c.chunkSize {
 			if i%100 == 0 {
 				fmt.Printf("Accumulated %d flows in file %d\n", i, fileCount)
 			}
-			sliceOfFlows[i] = <-channel
+			chunk[i] = <-channel // read data from UDP socket
 			i++
 		}
 
-		//	This goroutine stores given data into file
-		go save(sliceOfFlows, fileCount)
+		//	store given data into file
+		if fileCount < maxInt {
+			fileCount++
+		} else {
+			fileCount = 0
+		}
+		go save(chunk, fileCount)
 	}
 }
 
-func save(sliceOfFlows []NetFlowV5, fileCount int) {
+func save(chunk [][]byte, fileCount int) {
 	defer RecoverAnyPanic("save")
 
 	//	create new file
@@ -117,7 +106,8 @@ func save(sliceOfFlows []NetFlowV5, fileCount int) {
 
 	//	feed file with JSON array of decoded flows
 	f.WriteString("[")
-	for _, flow := range sliceOfFlows {
+	for _, datagram := range chunk {
+		flow := decodeAsNetFlowV5(datagram)
 		jSon, err := json.Marshal(&flow)
 		if err != nil {
 			LogOnError("ServerConn", err)
@@ -126,4 +116,18 @@ func save(sliceOfFlows []NetFlowV5, fileCount int) {
 		f.WriteString(fmt.Sprintf("%s,", string(jSon)))
 	}
 	f.WriteString("{}]") //	dirty trick that makes JSON always valid
+}
+
+func decodeAsNetFlowV5(buf []byte) NetFlowV5 {
+	defer RecoverAnyPanic("decodeAsNetFlowV5")
+
+	//	create new container for data
+	var flow NetFlowV5
+	//	Decode NetFlow v5 header
+	flow.Header = DecodeHeader(buf)
+	//	Decode NetFlow v5 records
+	for c := uint16(0); c < flow.Header.Count; c++ {
+		flow.Records = append(flow.Records, DecodeRecord(buf, c))
+	}
+	return flow
 }
